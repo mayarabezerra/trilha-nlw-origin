f4bed5c7869af82e90042f71062780a2a74cbc92
Mayara Bezerra <83085157+mayarabezerra@users.noreply.github.com>
2021-11-01 19:37:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const linksSocialMedia = {
+    github : 'mayarabezerra',
+    youtube : 'RocketSeat',
+    facebook : 'RocketSeat',
+    instagram : 'm.brayner',
+    twitter : 'docinhobrava'
+   }
+   function changeSocialMediaLinks() {
+     for (let li of socialLinks.children) {
+         const social = (li.getAttribute('class')) /*guardar nome da rede social que colocou na class cada vez que o loop rodar*/
+         li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+     }
+   }
+   changeSocialMediaLinks()
+
+   function getGitHubProfileInfo() {
+       const url = `https://api.github.com/users/${linksSocialMedia.github}`
+       fetch(url)
+       .then( response => response.json())
+       .then (data => {
+           userName.textContent = data.name
+           userBio.textContent = data.bio
+           userLink.href = data.html_url
+           userImg.src = data.avatar_url
+           userLogin.textContent = data.login
+   })
+}
+   getGitHubProfileInfo()
